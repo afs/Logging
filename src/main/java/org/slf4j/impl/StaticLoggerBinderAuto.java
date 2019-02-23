27fc4bb369ad72857d325a4569a3225f3de93d42
Andy Seaborne <andy@seaborne.org>
2019-02-23 16:50:56 +0000

    StaticLoggerBinderAuto

---
+++ src/main/java/org/slf4j/impl/StaticLoggerBinderAuto.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.slf4j.impl;
+
+//public class StaticLoggerBinder_ {}
+
+import logging.impl.FmtSimpleFactory;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
+/**
+ * A SLF4J {@linkplain LoggerFactoryBinder} that to log4j if possible else JUL.
+ */
+public class StaticLoggerBinderAuto implements LoggerFactoryBinder {
+    // Rename to activate.
+    private static StaticLoggerBinderAuto SINGLETON = new StaticLoggerBinderAuto();
+    
+    public static final StaticLoggerBinderAuto getSingleton() {
+        return SINGLETON;
+    }
+
+    private void decide() {
+        
+    }
+    
+    // SLF4J checking mechanism.
+//    // to avoid constant folding by the compiler, this field must *not* be final
+//    public static String REQUESTED_API_VERSION = "1.7.0"; // !final
+
+    @Override
+    public ILoggerFactory getLoggerFactory() {
+        return new FmtSimpleFactory();
+    }
+
+    @Override
+    public String getLoggerFactoryClassStr() {
+        return FmtSimpleFactory.class.getName();
+    }
+}
